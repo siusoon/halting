8f24822ac8614cc162719f51a75f6a89a3088d7b
siusoon <siusoon@gmail.com>
2016-10-01 13:17:21 +0200

    Update halting.py

--- halting.py
+++ halting.py
@@ -1,13 +1,13 @@
 #ref1: http://sahandsaba.com/static/presentations/it-from-bit.html#/10
 #ref2: https://www.explainxkcd.com/wiki/index.php/1266:_Halting_Problem
 
-#Assume this is another program called programB
+#assume this is another program called programB
 programB= raw_input("Get input: ") 
 
 #assume the function 'DoesItHalt' is a procedure that solves the halting problem
 if DoesItHalt(programB):  
-	print "No"  
- 	while True:  #performs as infinite loops i.e not halt
+	print "Yes"  
+ 	while (True):  #performs as infinite loops i.e not halt
 		pass 
 else:
-	print "Yes" #performs as an end i.e halt
+	print "No" #performs as an end i.e halt
